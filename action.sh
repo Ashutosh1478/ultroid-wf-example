41b0b695b735eb39f93334f0ad8870f014ec763b
CyrusXD <79554993+Ashutosh1478@users.noreply.github.com>
2022-04-01 12:30:39 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,4 +1,5 @@
-git clone -b v0.5.1 https://github.com/TeamUltroid/Ultroid /root/TeamUltroid
+branch=v0.5.1
+git clone -b $branch https://github.com/TeamUltroid/Ultroid /root/TeamUltroid
 cp ultroid/.env /root/TeamUltroid/.env
 cd /root/TeamUltroid
 docker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroid
